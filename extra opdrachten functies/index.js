a74c807258d478b56f6eefef38634c0cd2b8ca7f
vancapelledavid <david.van.capelle@gmail.com>
2021-02-21 23:38:47 +0100

    Add files via upload

---
+++ extra opdrachten functies/index.js
@@ -0,0 +1,25 @@
+function createPizza() {
+    console.log('make pizzabottom');
+    console.log('add topping');
+    console.log('put pizza in oven')
+}
+createPizza();
+createPizza();
+createPizza();
+
+
+function createSushi() {
+    console.log('make rice');
+    console.log('add topping');
+    console.log('roll stuff in seaweed leaf')
+}
+createSushi()
+
+//3x sushi 2x pizza
+
+createSushi();
+createSushi();
+createSushi();
+
+createPizza();
+createPizza();
